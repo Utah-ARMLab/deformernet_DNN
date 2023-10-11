9c8887ecbdf666eab1dc365a0a2cc3c55100144c
Bao <yuan.truyenbao@gmail.com>
2023-10-10 21:03:31 -0600

    minor commit

--- bimanual/dataset_loader.py
+++ bimanual/dataset_loader.py
@@ -10,12 +10,10 @@ import sklearn
 
                        
 
-
-
 class SingleBoxDataset(Dataset):
     """Shape servo dataset."""
     '''
-    Dataset for surgical setup task
+    Dataset for bimanual DeformerNet training.
     '''
 
     def __init__(self, percentage = 1.0, dataset_path=None):
@@ -75,4 +73,67 @@ class SingleBoxDataset(Dataset):
 
         return sample   
 
+   
+   
+class SingleBoxDatasetAllObjects(Dataset):
+    """Shape servo dataset."""
+    '''
+    Dataset for bimanual DeformerNet training for ALL OBJECTS.
+    '''
+
+    def __init__(self, percentage = 1.0, dataset_path=None):
+        """
+        Args:
+
+        """ 
+
+        # self.dataset_path = "/home/baothach/shape_servo_data/bimanual/box/processed_data/"
+        # self.dataset_path = "/home/baothach/shape_servo_data/bimanual/multi_boxes_1000Pa/processed_data"
+        # self.dataset_path = "/home/baothach/shape_servo_data/bimanual/multi_cylinders_1000Pa/processed_data"
+        # self.dataset_path = "/home/baothach/shape_servo_data/tube_connect/cylinder/processed_data"
+        self.dataset_path = dataset_path
+
+        self.filenames = os.listdir(self.dataset_path)
         
+        if percentage != 1.0:
+            self.filenames = os.listdir(self.dataset_path)[:int(percentage*len(self.filenames))]
+ 
+
+    
+    def load_pickle_data(self, filename):
+        with open(os.path.join(self.dataset_path, filename), 'rb') as handle:
+            return pickle.load(handle)            
+
+    def __len__(self):
+        return len(self.filenames)
+
+    def __getitem__(self, idx):
+        
+        # sample = self.load_pickle_data(self.filenames[idx])
+        
+        # # Bimanual
+        # pc = torch.tensor(sample["partial pcs"][0]).float()   # original partial point cloud
+        # pc_goal = torch.tensor(sample["partial pcs"][1]).float()      
+        # position = (torch.tensor(sample["positions"])*1000).float()
+        
+
+        # sample = {"pcs": (pc, pc_goal), "positions": position}     
+
+
+        # return sample    
+
+        sample = self.load_pickle_data(self.filenames[idx])
+        
+        pc = torch.tensor(sample["partial pcs"][0]).float()   # original partial point cloud
+        pc_goal = torch.tensor(sample["partial pcs"][1]).float()      
+
+        # position = torch.tensor(sample["pos"]).squeeze().float()
+        position = (torch.tensor(sample["pos"])*1000).squeeze().float()
+        rot_mat_1 = torch.tensor(sample["rot"][0]).float()
+        rot_mat_2 = torch.tensor(sample["rot"][1]).float()
+        sample = {"pcs": (pc, pc_goal), "pos": position, "rot_1": rot_mat_1,
+                  "rot_2": rot_mat_2}   
+
+
+
+        return sample      
